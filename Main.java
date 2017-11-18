391740433484cfe0fbb0958316a0d8d65f8d577d
Alexandr Kalambet <Alexandr.Kalambet@gmail.com>
2017-11-18 12:21:26 +0300

    LVLUP, 2 курс, урок 1

---
+++ Main.java
@@ -0,0 +1,11 @@
+package ru.levelup.lesson1;
+
+/**
+ * Created by admin on 17.11.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
